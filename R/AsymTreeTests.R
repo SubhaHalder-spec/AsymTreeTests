cf4c6fda9c92073c99cc51760eb4390a76c96efc
Subha Halder <133703969+SubhaHalder-spec@users.noreply.github.com>
2025-08-21 11:27:13 +0530

    Add files via upload

---
+++ R/AsymTreeTests.R
@@ -0,0 +1,127 @@
+#' T1 test for testing equality means against tree ordered alternatives in one-way ANOVA
+#' @export
+#' @param sample_data list
+#' @param significance_level numeric
+#' @return Critical value numeric
+#' @return Test statistic value numeric
+#' @return Result Character
+#' @details Testing of H0:mu0 = mu1 = ... = muk vs H1:mu0 <= mui (at least one strict inequality), for all i >= 1, where mui represents the population means of the i-th treatment. The input consists of two variables: sample_data and significance_level. The output consists of the critical value, the T1 test statistic value, and the result, which indicates whether to reject or not reject the null hypothesis.
+#' @importFrom MASS mvrnorm
+#' @importFrom stats quantile rnorm var
+#' @export
+#' @author Subha Halder
+#' @examples
+#' # Generate data
+#' set.seed(456)
+#' control <- rnorm(20, mean = 5)
+#' treatment1 <- rnorm(15, mean = 7)
+#' treatment2 <- rnorm(10, mean = 6)
+#'
+#' sample_data <- list(control, treatment1, treatment2)
+#'
+#' # Run LRT at 5% significance level
+#' T1(sample_data, 0.05)
+T1 <- function(sample_data, significance_level){
+  set.seed(456)
+  sample_data <- lapply(sample_data, function(x) x[!is.na(x)])
+  num_samples = 100000
+  num_datasets <- length(sample_data)
+  n <- sapply(sample_data, length)
+  proportions <- n / sum(n)
+  var_data <- sapply(1:num_datasets, function(j) var(sample_data[[j]]))
+  b_sq <- proportions[1] * var_data[-1] + proportions[-1] * var_data[1]
+  s <- num_datasets - 1
+  u_t <- matrix(0, s, s)
+  l_t <- matrix(0, s, s)
+  B <- diag(sqrt(b_sq))
+
+  for (i in 1:(s - 1)) {
+    for (j in (i + 1):s) {
+      u_t[i, j] <- sqrt(proportions[i + 1] * proportions[j + 1]) * var_data[1]
+      l_t[j, i] <- u_t[i, j]
+    }
+  }
+  cov_matrix <- u_t + l_t + B %*% t(B)
+  D <- solve(B) %*% cov_matrix %*% solve(B)
+  D_star_amax_values <- numeric(num_samples)
+  for (k in 1:num_samples) {
+    bootstrap_s <- MASS::mvrnorm(n = 1, mu = rep(0, s), Sigma = D)
+    D_star_amax_values[k] <- max(bootstrap_s)
+  }
+  quantile_value <- quantile(sort(D_star_amax_values), probs = 1-significance_level)
+  D_Amax <- max(sapply(2:num_datasets, function(j) (mean(sample_data[[j]]) - mean(sample_data[[1]])) /
+                         sqrt((var(sample_data[[j]]) / length(sample_data[[j]])) +
+                                (var(sample_data[[1]]) / length(sample_data[[1]])))))
+  if (D_Amax > quantile_value) {
+    result <- "Reject null hypothesis"
+  } else {
+    result <- "Do not reject null hypothesis"
+  }
+  return(paste("Critical value:", quantile_value, "; D_AMax Test statistic:", D_Amax, "; Result:", result))
+}
+
+
+#' T2 test for testing equality means against tree ordered alternatives in one-way ANOVA
+#' @export
+#' @param sample_data list
+#' @param significance_level numeric
+#' @return Critical value numeric
+#' @return Test statistic value numeric
+#' @return Result Character
+#' @details Testing of H_0:mu0 = mu1 = ... = muk vs H1:mu0 <= mui (at least one strict inequality), for all i >= 1, where mui represents the population means of the i-th treatment. The input consists of two variables: sample_data and significance_level. The output consists of the critical value, the T2 test statistic value, and the result, which indicates whether to reject or not reject the null hypothesis.
+#' @importFrom MASS mvrnorm
+#' @importFrom stats quantile rnorm var
+#' @export
+#' @author Subha Halder
+#' @examples
+#' # Generate data
+#' set.seed(456)
+#' control <- rnorm(20, mean = 5)
+#' treatment1 <- rnorm(15, mean = 7)
+#' treatment2 <- rnorm(10, mean = 6)
+#'
+#' sample_data <- list(control, treatment1, treatment2)
+#'
+#' # Run LRT at 5% significance level
+#' T2(sample_data, 0.05)
+T2 <- function(sample_data, significance_level){
+  set.seed(456)
+  sample_data <- lapply(sample_data, function(x) x[!is.na(x)])
+  num_samples = 100000
+  num_datasets <- length(sample_data)
+  n <- sapply(sample_data, length)
+  proportions <- n / sum(n)
+  var_data <- sapply(1:num_datasets, function(j) var(sample_data[[j]]))
+  b_sq <- proportions[1] * var_data[-1] + proportions[-1] * var_data[1]
+  s <- num_datasets - 1
+  u_t <- matrix(0, s, s)
+  l_t <- matrix(0, s, s)
+  B <- diag(sqrt(b_sq))
+
+  for (i in 1:(s - 1)) {
+    for (j in (i + 1):s) {
+      u_t[i, j] <- sqrt(proportions[i + 1] * proportions[j + 1]) * var_data[1]
+      l_t[j, i] <- u_t[i, j]
+    }
+  }
+  cov_matrix <- u_t + l_t + B %*% t(B)
+  D <- solve(B) %*% cov_matrix %*% solve(B)
+  D_star_amin_values <- numeric(num_samples)
+  for (k in 1:num_samples) {
+    bootstrap_s <- MASS::mvrnorm(n = 1, mu = rep(0, s), Sigma = D)
+    D_star_amin_values[k] <- min(bootstrap_s)
+  }
+  quantile_value <- quantile(sort(D_star_amin_values), probs = 1-significance_level)
+  D_Amin <- min(sapply(2:num_datasets, function(j) (mean(sample_data[[j]]) - mean(sample_data[[1]])) /
+                         sqrt((var(sample_data[[j]]) / length(sample_data[[j]])) +
+                                (var(sample_data[[1]]) / length(sample_data[[1]])))))
+  if (D_Amin > quantile_value) {
+    result <- "Reject null hypothesis"
+  } else {
+    result <- "Do not reject null hypothesis"
+  }
+  return(paste("Critical value:", quantile_value, "; D_AMax Test statistic:", D_Amin, "; Result:", result))
+}
+
+
+
